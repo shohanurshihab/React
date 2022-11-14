e545da326689fafa58e209a8b2939160270565ac
shohanurshihab <skshihab531@gmail.com>
2022-11-15 02:04:42 +0600

    spin-logo

---
+++ tryreact/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <br/>
+          <br/>
+          <br/>
+          This is Shihab trying to build a react App
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join SomaDhan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
